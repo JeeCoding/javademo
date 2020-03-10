0863a0fb6b0c9a33f9c386ebb275c14328d32168
RuPorcelain <251682553@qq.com>
2020-03-10 17:18:23 +0800

    java 多线程 线程池

---
+++ JavaMultiThread/src/main/java/example_4/Ali02_MyCallable.java
@@ -0,0 +1,19 @@
+package example_4;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @ClassName Ali02_MyCallable
+ * @Description TODO
+ * @Date 2020/3/10 17:14
+ * @Author huzh
+ * @Version 1.0
+ */
+public class Ali02_MyCallable implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(1000);
+        //返回执行当前 Callable 的线程名字
+        return Thread.currentThread().getName();
+    }
+}
