30ef39b82766624735f9db42c8d4e3d6c0442525
RuPorcelain <251682553@qq.com>
2020-03-10 16:34:56 +0800

    java 多线程 线程池创建

---
+++ JavaMultiThread/src/main/java/example_4/Ali01_MyRunnable.java
@@ -0,0 +1,42 @@
+package example_4;
+
+import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName Ali01_ThreadPoolExecutorDemo
+ * @Description TODO
+ * @Date 2020/3/10 15:43
+ * @Author huzh
+ * @Version 1.0
+ */
+public class Ali01_MyRunnable implements Runnable {
+    private String command;
+
+    public Ali01_MyRunnable(String s) {
+        this.command = s;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " Start. Time = " + new Date());
+        processCommand();
+        System.out.println(Thread.currentThread().getName() + " End. Time = " + new Date());
+    }
+
+    private void processCommand() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.command;
+    }
+
+}
