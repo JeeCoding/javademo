c6d4f4250ab84d835dd518db05a46aabd700644a
YueXian <251682553@qq.com>
2024-12-12 18:26:08 +0800

    ArrayList功能实现与测试

--- java-algorithms/data-structures/src/main/java/array_list/ArrayList.java
+++ java-algorithms/data-structures/src/main/java/array_list/ArrayList.java
@@ -1,6 +1,60 @@
 package array_list;
 
-public class ArrayList<E> {
+import java.util.Arrays;
 
-    private E[] data;
+public class ArrayList<E> implements List<E> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+    transient Object[] elementData;
+    private int size;
+
+    public ArrayList() {
+        this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+    }
+
+    @Override
+    public boolean add(E element) {
+        int minCapacity = size + 1;
+        if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+            minCapacity = Math.max(DEFAULT_CAPACITY, minCapacity);
+        }
+        if (minCapacity > elementData.length) {
+            int oldCapacity = elementData.length;
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            if (newCapacity < minCapacity) {
+                newCapacity = minCapacity;
+            }
+            elementData = Arrays.copyOf(elementData, newCapacity);
+        }
+        elementData[size++] = element;
+        return true;
+    }
+
+    @Override
+    public E remove(int index) {
+        E oldValue = (E) elementData[index];
+        int numMoved = size - index - 1;
+        if (numMoved > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index, numMoved);
+        }
+        elementData[--size] = null;
+        return oldValue;
+    }
+
+    @Override
+    public E get(int index) {
+        return (E) elementData[index];
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            list.add(i);
+        }
+        System.out.println("Original list: " + Arrays.toString(list.elementData));
+        list.remove(3);
+        System.out.println("After removing index 3: " + Arrays.toString(list.elementData));
+        System.out.println("Element at index 3: " + list.get(3));
+    }
 }
---
+++ java-algorithms/data-structures/src/main/java/array_list/List.java
@@ -0,0 +1,10 @@
+package array_list;
+
+public interface List<E> {
+
+    boolean add(E element);
+
+    E remove(int index);
+
+    E get(int index);
+}
